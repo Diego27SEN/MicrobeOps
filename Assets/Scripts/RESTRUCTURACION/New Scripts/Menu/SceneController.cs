84b4441d4578243d59d9e9f95c4b9b4819951ea8
Diego Benites <banitessebastian2decundaria@gmail.com>
2026-09-15 16:42:57 -0500

    ,

--- Assets/Scripts/RESTRUCTURACION/New Scripts/Menu/SceneController.cs
+++ Assets/Scripts/RESTRUCTURACION/New Scripts/Menu/SceneController.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
-public class ScenesController : MonoBehaviour
+public class SceneController : MonoBehaviour
 {
     [Header("Configuración de Transición")]
     [SerializeField] private Image fadeImage;
