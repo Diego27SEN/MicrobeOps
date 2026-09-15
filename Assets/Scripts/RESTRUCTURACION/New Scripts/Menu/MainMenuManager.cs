21d4a50571543931b2b23eabedfc23307f03166c
Diego Benites <banitessebastian2decundaria@gmail.com>
2026-09-15 16:34:07 -0500

    ,

--- Assets/Scripts/RESTRUCTURACION/New Scripts/Menu/MainMenuManager.cs
+++ Assets/Scripts/RESTRUCTURACION/New Scripts/Menu/MainMenuManager.cs
@@ -1,18 +1,18 @@
 using UnityEngine;
-using UnityEngine.UI; // Si usas Canvas tradicional
+using UnityEngine.UI; 
 
 public class MainMenuManager : MonoBehaviour
 {
     [SerializeField] private SceneController sceneController;
     [SerializeField] private string gameplaySceneName = "GameplayScene";
 
-    // Método que llamará el botón "Jugar" / "Start"
+
     public void OnPlayButtonClicked()
     {
         sceneController.LoadSceneByName(gameplaySceneName);
     }
 
-    // Método que llamará el botón "Salir"
+   
     public void OnExitButtonClicked()
     {
         sceneController.QuitGame();
--- Assets/Scripts/RESTRUCTURACION/New Scripts/Menu/SceneController.cs
+++ Assets/Scripts/RESTRUCTURACION/New Scripts/Menu/SceneController.cs
@@ -1,23 +1,79 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
-public class SceneController : MonoBehaviour
+public class ScenesController : MonoBehaviour
 {
-    // Carga una escena por su nombre exacto en el Build Settings
-    public void LoadSceneByName(string sceneName)
+    [Header("Configuración de Transición")]
+    [SerializeField] private Image fadeImage;
+    [SerializeField] private float duration = 2f;
+
+    private void Start()
     {
+        if (fadeImage != null)
+        {
+            Color c = fadeImage.color;
+            c.a = 0f;
+            fadeImage.color = c;
+            fadeImage.gameObject.SetActive(false);
+        }
+    }
+
+    public void MainPage() => StartTransition("OpeningMainScene");
+    public void SelectorScene() => StartTransition("SelectorScene");
+    public void Map1() => StartTransition("TestCinematic");
+    public void ModeScene() => StartTransition("ModeSelector");
+    public void CreditsScene() => StartTransition("CreditsScene");
+    public void Winning() => StartTransition("WinnerScene");
+
+    public void Quitgame()
+    {
+        StartCoroutine(QuitRoutine());
+    }
+
+    private void StartTransition(string sceneName)
+    {
+        StartCoroutine(FadeAndLoadRoutine(sceneName));
+    }
+
+    // Corrutina base para el efecto visual de Fade Out
+    private IEnumerator FadeOutRoutine()
+    {
+        if (fadeImage != null)
+        {
+            fadeImage.gameObject.SetActive(true);
+            float timer = 0f;
+            Color originalColor = fadeImage.color;
+
+            while (timer < duration)
+            {
+                // Usamos unscaledDeltaTime por si el juego está en pausa (Time.timeScale = 0)
+                timer += Time.unscaledDeltaTime;
+                float progress = timer / duration;
+
+                originalColor.a = Mathf.Lerp(0f, 1f, progress);
+                fadeImage.color = originalColor;
+
+                yield return null;
+            }
+        }
+        else
+        {
+            yield return new WaitForSecondsRealtime(duration);
+        }
+    }
+
+    private IEnumerator FadeAndLoadRoutine(string sceneName)
+    {
+        yield return StartCoroutine(FadeOutRoutine());
         SceneManager.LoadScene(sceneName);
     }
 
-    // Carga la escena según el índice en Build Settings
-    public void LoadSceneByIndex(int sceneIndex)
+    private IEnumerator QuitRoutine()
     {
-        SceneManager.LoadScene(sceneIndex);
-    }
+        yield return StartCoroutine(FadeOutRoutine());
 
-    // Salir del juego
-    public void QuitGame()
-    {
 #if UNITY_EDITOR
         UnityEditor.EditorApplication.isPlaying = false;
 #else
